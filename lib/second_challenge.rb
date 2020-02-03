e4aa73d2705a62ff56e3536b8728395e93a28ee7
Shervon Ceromain <shervonc@gmail.com>
2020-02-03 20:14:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,14 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
+      patriach => {:name => "Lord Montague", :age => "53"},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
+      :hero => {:name => "Romeo", :age => "15"},
+      :hero_friends => [
+      {:name =>'Benvolio',:age => "17", :attitude => "worried"},
+      {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
+                        ]
+
       }, 
     :capulet => {
       #your key/value pairs here!
